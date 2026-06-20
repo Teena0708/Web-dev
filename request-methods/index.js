ceb5645e012d07638c714320d516ac2b999e4f62
Teena0708 <yteena51@gmail.com>
2026-06-20 20:37:41 +0530

    Updated index.js

--- request-methods/index.js
+++ request-methods/index.js
@@ -9,6 +9,8 @@ const users = [{
 
 }];
 
+app.use(express.json());
+
 
 //we send query parameters in get request.
 app.get("/", function(req, res) {
@@ -31,9 +33,32 @@ app.get("/", function(req, res) {
 //we send body in post request
 app.post("/", function(req,res){
     const isHealthy = req.body.isHealthy;
-    users[0].kidneys.push{{
+    users[0].kidneys.push({
         healthy : isHealthy
-    }}
+    })
+    res.json({
+        msg:"done!"
+    })
+})
+
+app.put("/", function(req,res){
+    for(let i=0;i<users[0].kidneys.length;i++){
+        users[0].kidneys[i].healthy = true;
+    }
+    res.json({});
+})
+
+app.delete("/" ,function(req,res){
+    let newKidneys = [];
+    for(let i=0;i<users[0].kidneys.length;i++){
+        if(users[0].kidneys.healthy){
+            newKidneys.push({
+                healthy : true
+            })
+        }
+    }
+    users[0].kidneys = newKidneys;
+    res.json({msg : "done"})
 })
 
 app.listen(3000);
