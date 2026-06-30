f941a707b00d76e3c3ae455e99c83e5b272a8ac6
Teena0708 <yteena51@gmail.com>
2026-06-30 23:46:29 +0530

    added

---
+++ DataBase/Mongoose.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.json());
+mongoose.connect("mongodb+srv://admin:password@cluster0.xynnre4.mongodb.net/userappnew?retryWrites=true&w=majority");
+const User = mongoose.model('Users', { name: String, email: String, password: String });
+
+app.post('/signUp', async function(req, res) {
+    const username = req.body.username;
+    const email = req.body.email;
+    const name = req.body.name;
+
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+        return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const newUser = new User({
+        name: name,
+        email: email,
+        password: password
+    });
+
+    await newUser.save();
+    res.status(201).json({ message: 'User created successfully' });
+});
