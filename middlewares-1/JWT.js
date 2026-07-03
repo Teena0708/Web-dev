a7dfbb8309daec60d792df88b0266c74547c02a7
Teena0708 <yteena51@gmail.com>
2026-07-04 00:12:39 +0530

    added

--- middlewares-1/JWT.js
+++ middlewares-1/JWT.js
@@ -1,2 +1,41 @@
 //JWT can be used to generate , decoding , verify .
 
+const jwt = require("jsonwebtoken");
+const zod = require("zod");
+
+const jwtpassword = "mysecretpassword";
+
+const emailSchema = zod.string().email();
+const passwordSchema = zod.string().min(6);
+
+function generateToken(email, password) {
+    const emailValidation = emailSchema.safeParse(email);
+    const passwordValidation = passwordSchema.safeParse(password);
+    if(!emailValidation.success || !passwordValidation.success){
+        return null;
+    }
+    const signature = jwt.sign({
+        email},jwtpassword
+    )
+    return signature;
+}
+
+function verifyToken(token) {
+    const decoded = jwt.decode(token);
+    if(decoded){
+        return true;
+    }
+    else{
+        false
+    }
+}
+
+function verifyToken(token){
+    const verified = jwt.verify(token,jwtpassword);
+    if(verified){
+        return true;
+    }
+    else{
+        false
+    }
+}
